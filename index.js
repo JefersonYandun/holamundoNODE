e883d02ecb02c23afaf65eed073f75743de80bf7
JefersonYandun <jeferson.yandun237@ist17dejulio.edu.ec>
2024-04-30 11:04:52 -0500

    Correccion hola mundo del codigo node

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 var http=require('http');
 
 //A TRAVES DEL PORTOOCLO CREAMOS UN SERVIDOR Y LO ALMACENAMOS EN UN SERVER
-var server=http.createServer()
+var server=http.createServer();
 
 
 //METODO CON EL QUE SE PUEDE CONFIGURAR lo siguiente:
 function mensaje(peticion,resp){
     //1. el estadp del servidor a traves de un texto
     resp.writeHead(200,{'content-type':'text/plain'});
-    //2. el mensjae a mostrar
-    resp.writeHead("YA MANDEN A LAS CASAS !!!");
+    //2. el mensjae a mostrar   
+    resp.write("YA MANDEN A LAS CASAS !!!");
     //3. Finalizar respuesta
     resp.end();
 
